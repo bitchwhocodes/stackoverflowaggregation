cb789e69473ddd4f35f16626beb8eb0a23b9a648
bitchwhocodes <stacey.mulcahy@gmail.com>
2016-01-25 02:07:52 -0500

    added web jobs

--- webjobs/email/run.js
+++
@@ -1,6 +0,0 @@
-var request = require('request');
-request('http://taggedytagtag.azurewebsites.net/email/day', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("complete");
-  }
-})
